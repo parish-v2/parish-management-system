29a69c7f4f41de3c6fdf6b49a486bb1f3be4786f
Thaumaturgy <deweysia@gmail.com>
2019-02-12 02:08:02 +0800

    Add basic login and logout

--- parishsystem/settings.py
+++ parishsystem/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+LOGIN_REDIRECT_URL = 'registration-index'
+LOGIN_URL = 'login'
--- registration/urls.py
+++ registration/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 from .views import index
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', index, name='registration-index'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout')
 ]
