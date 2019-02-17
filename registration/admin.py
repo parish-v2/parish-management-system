8d2cf65f91a7dc4c70c390d56fe03783df231af4
Student <lerycibalio@gmail.com>
2019-02-17 18:06:40 +0800

    Register models

--- registration/admin.py
+++ registration/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import User
 from .forms import MyUserChangeForm
 from django.contrib.auth.admin import UserAdmin
-
+from sacrament.models import Baptism, Marriage, Confirmation, Invoice, InvoiceItems, ItemType, Profile, Minister, Sponsor, Schedule
 class MyUserAdmin(UserAdmin):
 	form = MyUserChangeForm
 
@@ -13,3 +13,14 @@ class MyUserAdmin(UserAdmin):
 	)
 
 admin.site.register(User, MyUserAdmin)
+
+admin.site.register(Baptism)
+admin.site.register(Marriage)
+admin.site.register(Confirmation)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItems)
+admin.site.register(ItemType)
+admin.site.register(Profile)
+admin.site.register(Minister)
+admin.site.register(Sponsor)
+admin.site.register(Schedule)
