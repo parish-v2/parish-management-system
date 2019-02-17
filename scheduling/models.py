7d3c58b5c5869056c708bc02f453751c74dd0118
Thaumaturgy <deweysia@gmail.com>
2019-02-17 20:44:50 +0800

    Fix indentation & variable changes in Schedule model

--- scheduling/models.py
+++ scheduling/models.py
@@ -3,41 +3,44 @@ from sacrament.models import Baptism, Confirmation, Marriage
 
 # Create your models here.
 class Schedule(models.Model):
-	SACRAMENT_TYPES = (
+	SACRAMENT_TYPES_CHOICES = (
 		(1, 'Baptism'),
 		(2, 'Confirmation'),
 		(3, 'Marriage'),
 	)
 
-    sacrament_type = models.SmallIntegerField(choices=SACRAMENT_TYPES, null=True, blank=True)
-    # TODO: add schedule classes then push to models dev-
+	sacrament_type = models.PositiveSmallIntegerField(choices=SACRAMENT_TYPES_CHOICES, null=True, blank=True)
+	# TODO: add schedule classes then push to models dev-
 
-    # Nullable in case the schedule is connected to 
-    # a sacrament.
-    baptism = models.ForeignKey(
-        Baptism,
-        on_delete=models.PROTECT,
-        related_name='schedules',
-        null=True,
-        blank=True,
-    )
-    marriage = models.ForeignKey(
-        Marriage,
-        on_delete=models.PROTECT,
-        related_name='schedules',
-        null=True,
-        blank=True,
-    )
-    confirmation = models.ForeignKey(
-        Confirmation,
-        on_delete=models.PROTECT,
-        related_name='schedules',
-        null=True,
-        blank=True,
-    )
+	# Nullable in case the schedule is connected to
+	# a sacrament.
 
-    # title of the event
-    title = models.CharField(max_length=255)
-    details = models.CharField(max_length=255)
-    start_date_time = models.DateTimeField()
-    end_date_time = models.DateTimeField()
+	baptism = models.ForeignKey(
+		Baptism,
+		on_delete=models.PROTECT,
+		related_name='schedules',
+		null=True,
+		blank=True,
+	)
+
+	marriage = models.ForeignKey(
+		Marriage,
+		on_delete=models.PROTECT,
+		related_name='schedules',
+		null=True,
+		blank=True,
+	)
+
+	confirmation = models.ForeignKey(
+		Confirmation,
+		on_delete=models.PROTECT,
+		related_name='schedules',
+		null=True,
+		blank=True,
+	)
+
+	# title of the event
+	title = models.CharField(max_length=255)
+	details = models.CharField(max_length=255)
+	start_date_time = models.DateTimeField()
+	end_date_time = models.DateTimeField()
