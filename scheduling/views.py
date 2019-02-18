fb123fd980511ffe5511bed911bdbfe5bb265622
Thaumaturgy <deweysia@gmail.com>
2019-02-18 19:34:10 +0800

    Add basic scheduling urls and CRUD views

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from .models import Schedule
 
 # Create your views here.
 def index(request):
 	return render(request, template_name='scheduling/index.html')
+
+class ScheduleDetailView(DetailView):
+	model = Schedule
+	template_name = "scheduling/schedule_detail.html"
+
+class ScheduleListView(ListView):
+	model = Schedule
+	template_name = "scheduling/schedule_list.html"
+
+class ScheduleCreateView(CreateView):
+	model = Schedule
+	template_name = "scheduling/schedule_create_form.html"
+	fields = ['title', 'details', 'start_date_time', 'end_date_time']
+
+class ScheduleUpdateView(UpdateView):
+	model = Schedule
+	template_name = "scheduling/schedule_update_form.html"
+	fields = ['title', 'details', 'start_date_time', 'end_date_time']
+
