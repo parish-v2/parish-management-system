061de9f29019db0c7dc8064c0f8b9fcb80a9ade6
justinhenrylo <lojustinhenry@gmail.com>
2019-02-19 12:21:20 +0800

    finish add_item_type view

--- finance/views.py
+++ finance/views.py
@@ -5,9 +5,14 @@ from .models import ItemType
 
 def add_item_type(request):
     context={}
+    context['ItemType']= ItemType.objects.all()
+    context['ItemTypeModelForm'] = ItemTypeModelForm()
     if(request.method=="POST"):
-        pass
+        itemtype_form = ItemTypeModelForm(request.POST)
+        if(itemtype_form.is_valid()):
+            itemtype_form.save()
+            return render(request,"finance/item_type.html",context)
+        else:
+            return render(request,"finance/item_type.html",context)
     else:
-        context['ItemTypeModelForm'] = ItemTypeModelForm()
-        context['ItemType']= ItemType.objects.all()
         return render(request,"finance/item_type.html",context)
