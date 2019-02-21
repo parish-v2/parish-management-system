5c6d989c1ea87bf104471bfaa8d87db2d79db9f9
Student <lerycibalio@gmail.com>
2019-02-21 19:34:12 +0800

    Add sponsors in baptism record modal view and layout

--- sacrament/tables.py
+++ sacrament/tables.py
@@ -15,10 +15,10 @@ class BaptismTable(tables.Table):
     class Meta:
         template_name = 'django_tables2/bootstrap.html'
         row_attrs = {
-            'class': 'selectable-row table-sm'
+            'class': 'selectable-row table-sm us'
         }
         model = Baptism
-        
+        exclude = ('target_price','minister','registry_number','page_number','record_number', 'remarks' )  
         sequence = ('id', 'profile', 'status', 'date', 'target_price', 'minister', 'legitimacy')
         attrs = {'class': 'table table-hover selectable-table table-bordered'}
         template_name = 'sacrament/table.html'
